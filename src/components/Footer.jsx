bcc917251e07103043249318768050949d84f97c
Vaagishwar <vaagishwar77@gmail.com>
2026-05-20 18:43:08 +0530

    fixed footer margin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-sm text-center mb-6 text-mist-500'>Made through passion 🍉</div>
+    <div className='text-sm text-center mb-15 text-mist-500'>Made through passion 🍉</div>
   )
 }
 
